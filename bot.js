374a1bdcf948ad61b47171e59cb9b74dfea5f8e4
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2019-02-02 14:58:06 -0200

    gerenciando torneio

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
 var Discord = require('discord.js');
 var logger = require('winston');
+var auth = require('./auth.json');
 
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 
+var torneios = [];
+
 var bot = new Discord.Client();
-console.log("entrou");
 
 bot.on('ready', function (evt) {
     logger.info('Connected');
@@ -16,30 +18,185 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('message', function (message) {
+    var userName = message.author.username + '#' + message.author.discriminator;
+
     if (message.content.substring(0, 1) == '!') {
         var args = message.content.substring(1).split(' ');
         var cmd = args[0];
+        var param = "";
+
+        if (args.length > 1)
+            param = args[1];
 
         args = args.splice(1);
 
         switch (message.channel.name) {
             case 'treino':
-                handleTreino(cmd, message);
+                handleTreino(cmd, message, param, userName);
                 break;
         }
     }
 });
 
-function handleTreino(cmd, message) {
+function handleTreino(cmd, message, param, userName) {
     switch (cmd) {
         case 'treino':
-            message.channel.members.forEach(member => {
-                if (member != null && member != undefined) {
-                    member.createDM()
-                        .then(x => x.send('Bora treinar'));
-                }
-            });
+            messageAllPlayers(message.channel.members, 'Bora treinar');
+            break;
+
+        case 'criar-torneio':
+            createTournament(message, param);
+            break;
+
+        case 'participar':
+            joinTournament(message, param, userName);
+            break;
+
+        case 'listar-torneios':
+            var tournaments = listTournaments();
+            if (tournaments.trim().length == 0)
+                message.reply("Não há torneios criados");
+            else
+                message.reply("Os torneios criados que estão em andamento são \n" + tournaments);
+            break;
+
+        case 'sair':
+            quitTournament(message, param, userName);
+            break;
+
+        case 'finalizar-torneio':
+            finishTournament(message, param, userName);
+            break;
+
+        case 'listar-participantes':
+            var players = listPlayerFromTournament(param);
+
+            if (players.trim().length == 0)
+                message.reply("Não há participantes nesse torneio");
+            else
+                message.reply("Os participantes do torneio são \n" + players);
+            break;
+
+        case 'chamar-torneio':
+            callPlayers(message, param);
+            break;
     }
 }
 
-bot.login(process.env.TOKEN);
+function messageMember(member, message) {
+    member.createDM()
+        .then(x => x.send(message));
+}
+
+function messageAllPlayers(members, message) {
+    members.forEach(member => {
+        if (member != null && member != undefined) {
+            member.createDM()
+                .then(x => x.send(message));
+        }
+    });
+}
+
+function createTournament(message, name) {
+    torneios.push({
+        name: name.trim(),
+        players: [],
+        description: "",
+        isRunning: true
+    });
+    message.reply("Torneio '" + name + "' criado com sucesso");
+}
+
+function joinTournament(message, tournamentName, userName) {
+    var tournament = torneios.find(x => x.name == tournamentName && x.isRunning);
+
+    if (tournament != undefined && tournament != null) {
+        // torneio existe, adiciona player no torneio
+        if (tournament.players.indexOf(userName == -1)) {
+            tournament.players.push(userName);
+            message.reply("Você foi incluído no torneio: " + tournamentName);
+        }
+        else
+            message.reply("Você já está no torneio");
+    }
+    else
+        message.reply("Não foi possível localizar o torneio, o enzo deve ter feito cagado, desculpe");
+}
+
+function listTournaments() {
+    var tournaments = "";
+
+    torneios.forEach(tourny => {
+        if (tourny.isRunning)
+            tournaments += tourny.name + "\n";
+    });
+
+    return tournaments;
+}
+
+function quitTournament(message, tournamentName, userName) {
+    var tournament = torneios.find(x => x.name == tournamentName && x.isRunning);
+
+    if (tournament != undefined && tournament != null) {
+        var index = tournament.players.indexOf(userName);
+        if (index != -1) {
+            tournament.players.splice(index, 1);
+            message.reply("Você saiu do torneio " + tournamentName);
+        }
+        else
+            message.reply("Você não está nesse torneio");
+    }
+    else
+        message.reply("Não foi possível localizar o torneio, o enzo deve ter feito cagado, desculpe");
+}
+
+function finishTournament(message, tournamentName, userName) {
+    var tournament = torneios.find(x => x.name == tournamentName && x.isRunning);
+
+    if (tournament != undefined && tournament != null) {
+        if (tournament.isRunning) {
+            tournament.isRunning = false;
+            message.reply("Torneio " + tournamentName + " foi finalizado");
+        }
+        else
+            message.reply("Torneio já foi finalizado");
+    }
+    else
+        message.reply("Não foi possível localizar o torneio, o enzo deve ter feito cagado, desculpe");
+}
+
+function listPlayerFromTournament(tournamentName) {
+    var tournament = torneios.find(x => x.name == tournamentName && x.isRunning);
+
+    if (tournament != undefined && tournament != null) {
+        var players = "";
+
+        tournament.players.forEach(player => {
+            players += player + "\n";
+        });
+
+        return players;
+    }
+    else
+        message.reply("Não foi possível localizar o torneio, o enzo deve ter feito cagado, desculpe");
+
+    return "";
+}
+
+function callPlayers(message, tournamentName) {
+    var tournament = torneios.find(x => x.name == tournamentName && x.isRunning);
+
+    if (tournament != undefined && tournament != null) {
+        tournament.players.forEach(player => {
+            var member = message.channel.members.find(x => (x.user.username + '#' + x.user.discriminator) == player);
+
+            if (member != undefined && member != null) {
+                messageMember(member, "O Torneio " + tournamentName + " está para começar, fique atento");
+            }
+        });
+    }
+    else
+        message.reply("Não foi possível localizar o torneio, o enzo deve ter feito cagado, desculpe");
+}
+
+bot.login(process.env.TOKEN || auth.token);
